445ddaa85469c5de3d7bc2748e5e4b666e571a17
Jay <honeycutt.james@gmail.com>
2023-05-10 15:13:50 -0400

    Started to enable wslv2

---
+++ PowerShell/Computers/Enable-WSLv2/Enable-wslv2.ps1
@@ -0,0 +1,3 @@
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
